a55839b6c94f22fdc549fec2dd05caf60ae4a2bc
benibienz <benibienz@gmail.com>
2017-08-03 11:20:48 +0100

    Switch to roboschool, play() function added, reformatting

--- baselines/pposgd/run_roboschool.py
+++ baselines/pposgd/run_roboschool.py
@@ -2,11 +2,11 @@
 from baselines.common import set_global_seeds, tf_util as U
 from baselines import bench
 import os.path as osp
-import gym, logging
+import gym, logging, roboschool
 from baselines import logger
 import sys
 
-def train(env_id, num_timesteps, seed):
+def train(env_id, num_eps, seed):
     from baselines.pposgd import mlp_policy, pposgd_simple
     U.make_session(num_cpu=1).__enter__()
     logger.session().__enter__()
@@ -15,20 +15,33 @@ def train(env_id, num_timesteps, seed):
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
             hid_size=64, num_hid_layers=2)
-    env = bench.Monitor(env, osp.join(logger.get_dir(), "monitor.json"))
+    env = bench.Monitor(env, osp.join(logger.get_dir(), "monitor.json"), allow_early_resets=True)
     env.seed(seed)
     gym.logger.setLevel(logging.WARN)
-    pposgd_simple.learn(env, policy_fn, 
-            max_timesteps=num_timesteps,
+    pi = pposgd_simple.learn(env, policy_fn,
+            max_episodes=num_eps,
             timesteps_per_batch=2048,
             clip_param=0.2, entcoeff=0.0,
             optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
-            gamma=0.99, lam=0.95,
+            gamma=0.99, lam=0.95
         )
+    input()
+    play(env, 10, pi)
     env.close()
 
+def play(env, num_runs, policy_fn):
+
+    for _ in range(num_runs):
+        ob = env.reset()
+        done = False
+        while not done:
+            ac, _ = policy_fn.act(True, ob)
+            ob, rew, done, _ = env.step(ac)
+            env.render()
+
+
 def main():
-    train('Hopper-v1', num_timesteps=1e6, seed=0)
+    train('RoboschoolInvertedPendulumSwingup-v1', num_eps=2000, seed=0)
 
 
 if __name__ == '__main__':
